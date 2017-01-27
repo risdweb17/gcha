f5a672655fc87ca848e63014d5c0ad92a9e0b132
ghongcha <gcha@risd.edu>
2017-01-27 16:07:26 -0500

    five pages now

--- project 3/js/behavior 3.js
+++
@@ -1,7 +0,0 @@
-h1 {
-	color: cyan;
-	font-size: 200px;
-	position: absolute;
-	top: -150px;
-	left: 50px;
-}
---
+++ project 3/js/misc behavior.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){}
+	$('h3').clickon(function(){
+		$('iframe').ScrollTo({
+		    duration: 2000,
+		    easing: 'linear'
+	}
+});
